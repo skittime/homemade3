5732b9d85ac0a736002c9e128a77b8238b1753f9
skittime <skittime@gmail.com>
2020-09-12 00:18:26 +0500

    Условие 3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,22 @@
 package com.company;
 
+
+
 public class Main {
 
+
+
     public static void main(String[] args) {
+        Phone copy0 = new Phone();
+        Phone copy1 = new Phone();
+        Phone copy2 = new Phone();
+        System.out.println(copy0.number);
+        System.out.println(copy1.model);
+        System.out.println(copy2.weight);
+        copy0.receiveCall();
+        copy0.getNumber();
+        ;
+
 
     }
 }
--- src/com/company/Phone.java
+++ src/com/company/Phone.java
@@ -1,12 +1,30 @@
 package com.company;
 
 public class Phone {
-    int number;
-    String model, name;
-    double weight;
+    long number = 9996478812l;
+    String model = "Samsung", name = "Alex";
+
+    public Phone(long number, String model) {
+        this.number = number;
+        this.model = model;
+    }
+
+    double weight = 0.254;
+
+    public Phone() {
+    }
+
+    public Phone(long number, String model, double weight) {
+        this.number = number;
+        this.model = model;
+        this.weight = weight;
+    }
 
 
-    public void receiveCall(String name){
-        System.out.println("Зваонит " + name);
+    public void receiveCall(){
+        System.out.println("Звонит " + name);
+    }
+    public void getNumber (){
+        System.out.println(number);
     }
 }
