eefd319ff46687eeadda230e5f6f471a14815040
skittime <skittime@gmail.com>
2020-09-13 03:33:57 +0500

    Вызов метод sendMessage c параметрами

--- src/x5/Phone/Main.java
+++ src/x5/Phone/Main.java
@@ -1,21 +1,20 @@
 package x5.Phone;
 
 
-
 public class Main {
-
 
 
     public static void main(String[] args) {
         Phone copy0 = new Phone();
         Phone copy1 = new Phone();
         Phone copy2 = new Phone();
-        System.out.println(copy0.number);
-        System.out.println(copy1.model);
-        System.out.println(copy2.weight);
+        System.out.println("Номер телефона: " +copy0.numberOfCall);
+        System.out.println( "Модель телефона: " + copy1.model);
+        System.out.println("Вес телефона: " + copy2.weight);
         copy0.receiveCall();
         copy0.getNumber();
-       // Phone methodPhone = new Phone(6465465, "Xiaomi");
-
+        Phone methodPhone = new Phone();
+        methodPhone.receiveCall(999546456L, "Erzhan");
+        methodPhone.sendMessage(8498494984L, 65465465L, 1561656565L);
     }
 }
