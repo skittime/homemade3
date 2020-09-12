a4a1553ff8c9dcb36d71707b1a23a77d6631a647
skittime <skittime@gmail.com>
2020-09-13 03:33:47 +0500

    Вызов метод sendMessage c параметрами

--- src/x5/Phone/Phone.java
+++ src/x5/Phone/Phone.java
@@ -26,15 +26,20 @@ public class Phone {
     public void receiveCall() {
         this.numberOfCall += numberOfCall;
         this.name += name;
-        System.out.println("Звонит " + numberOfCall + name);
+        System.out.println("Звонит " + numberOfCall + " "  + name);
     }
 
     public void receiveCall(long numberOfCall, String name) {
         this.numberOfCall += numberOfCall;
         this.name += name;
-        System.out.println("Звонит " + numberOfCall+ " " + name);
+        System.out.println("Звонит " + numberOfCall + " " + name);
     }
+
     public void getNumber() {
         System.out.println(numberOfCall);
+    }
+
+    public void sendMessage(long numberOfCall0, long numberOfCall1, long numberOfCall2) {
+        System.out.println("Номера для сообщений " + numberOfCall0+ " " + numberOfCall1+ " "  + numberOfCall2);
     }
 }
